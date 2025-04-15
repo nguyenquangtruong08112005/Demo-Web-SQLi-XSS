c5bc9c6090ce4e0de9b100fd448b0e36d5b719bd
Nguyễn Quang Trường <149094055+nguyenquangtruong08112005@users.noreply.github.com>
2025-04-15 13:32:48 +0700

    Hide Database Error

--- config.php
+++ config.php
@@ -5,6 +5,9 @@ $user = 'root';
 $pass = '';
 $dbname = 'cellphoneshop';
 
+error_reporting(0);
+ini_set('display_errors', 0);
+
 $conn = new mysqli($host, $user, $pass, $dbname);
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
